6bade306fd18d2d09fdcc02d98379370d4a2d19e
Parham Saravani <parhamsaravani83@gmail.com>
2026-06-07 22:23:21 +0330

    taking fall in done

--- src/js/fall.js
+++ src/js/fall.js
@@ -1,1 +1,107 @@
 import '../style.css'
+
+const darkBtn = document.querySelector('.dark-btn')
+const lightBtn = document.querySelector('.light-btn')
+
+const takeFallBtn = document.querySelector('.take-fall')
+
+const poetNumberText = document.querySelector('.fall-poet-number')
+const poetText = document.querySelector('.fall-poet')
+const poetSummeryText = document.querySelector('.fall-summery')
+
+const fallPoetContent = document.querySelector('.fall-poet-content')
+const fallSummaryContent = document.querySelector('.fall-summary-content')
+
+const fallPoetContainer = document.querySelector('.fall-poet-container')
+const fallSummaryContainer = document.querySelector('.fall-summary-container')
+
+const loaderContainer = document.querySelector('.loader-container')
+
+const onPageLoad = () => {
+    themeHandler();
+}
+
+const fetchFall = () => {
+    showLoader();
+    fetch('https://api.ganjoor.net/api/ganjoor/hafez/faal')
+        .then(res => res.json())
+        .then(data => {
+            createFallHandler(data);
+        })
+        .catch(() => {
+            errorHandler();
+        })
+        .finally(() => {
+            hideLoader()
+        })
+}
+
+const createFallHandler = (data) => {
+    poetNumberText.innerHTML = '';
+    poetText.innerHTML = '';
+    poetSummeryText.innerHTML = '';
+    poetNumberText.insertAdjacentHTML('beforeend', `${data.title} از دیوان حافظ`)
+    poetText.insertAdjacentHTML('beforeend', data.htmlText)
+    poetSummeryText.insertAdjacentHTML('beforeend', data.poemSummary)
+
+}
+
+const errorHandler = () => {
+    fallPoetContainer.classList.add('hidden')
+    fallSummaryContainer.classList.add('hidden')
+}
+
+const showLoader = () => {
+    fallSummaryContainer.classList.add('hidden')
+    fallPoetContainer.classList.remove('hidden')
+
+    fallPoetContent.classList.add('hidden')
+
+    loaderContainer.classList.remove('hidden')
+    loaderContainer.classList.add('h-60')
+    loaderContainer.innerHTML = '<div class="loader w-4 rounded-full animate-loader aspect-square"></div>'
+}
+const hideLoader = () => {
+    fallPoetContent.classList.remove('hidden')
+    fallSummaryContainer.classList.remove('hidden')
+    fallPoetContent.classList.add('flex')
+
+    loaderContainer.classList.remove('h-60')
+    loaderContainer.classList.add('hidden')
+}
+
+//! theme Handler
+const themeHandler = (isDarkMode) => {
+    const status = JSON.parse(localStorage.getItem('isDarkMode'));
+    if (!status) {
+        lightTheme();
+    } else {
+        darkTheme();
+    }
+}
+const darkTheme = () => {
+    lightBtn.classList.remove('hidden')
+    lightBtn.classList.add('flex')
+
+    darkBtn.classList.add('hidden')
+    darkBtn.classList.remove('flex')
+    document.documentElement.classList.add('dark');
+    saveThemeInLocalStorage('true')
+
+}
+const lightTheme = () => {
+    darkBtn.classList.remove('hidden')
+    darkBtn.classList.add('flex')
+    lightBtn.classList.add('hidden')
+    document.documentElement.classList.remove('dark');
+    saveThemeInLocalStorage('false')
+}
+const saveThemeInLocalStorage = (isDarkMode) => {
+    localStorage.setItem('isDarkMode', isDarkMode)
+}
+
+
+document.addEventListener('DOMContentLoaded', onPageLoad)
+takeFallBtn.addEventListener('click', fetchFall)
+darkBtn.addEventListener('click', darkTheme)
+lightBtn.addEventListener('click', lightTheme);
--- src/js/poets.js
+++ src/js/poets.js
@@ -10,10 +10,7 @@ const loaderContainer = document.querySelector('.loader-container')
 
 const sortBtn = document.querySelector('.sorte-btn')
 const sortItems = document.querySelectorAll('.sort-by-century-items')
-const sortContainer = document.querySelector('.sort-items')
-
-console.log(sortItems);
-
+const sortContainer = document.querySelector('.sort-container')
 
 const url = 'https://api.ganjoor.net';
 let count = 1;
@@ -34,7 +31,7 @@ const fetchPoetsData = () => {
             poetsElementHandler();
         })
         .catch(() => {
-            
+
         })
         .finally(() => {
             hideLoader();
@@ -45,7 +42,7 @@ const poetsElementHandler = () => {
     totalpoets.slice(0, 23 * count).forEach(item => {
         poetsContainer.insertAdjacentHTML('beforeend',
             `
-            <div class="relative gap-4 w-51 py-4 rounded-xl dark:bg-dark-card bg-light-card shadow-xl border-2 dark:border-dark-border border-light-border">
+            <div class="relative gap-4 w-51 max-sm:w-55 max-md:w-50 max-lg:w-44 max-xl:w-38.5 max-2xl:w-36 py-4 rounded-xl dark:bg-dark-card bg-light-card shadow-xl border-2 dark:border-dark-border border-light-border">
                 <div class="h-full flex flex-col items-center gap-2">
                     <img class="size-26 rounded-full popular-poet-img cursor-pointer" src="${url}${item.imageUrl}" alt="" data-poet="${item.fullUrl}">
                     <p class="dark:text-dark-text text-light-text font-vazir text-card-header font-bold popular-poet-title cursor-pointer" data-poet="${item.fullUrl}">${item.name}</p>
@@ -68,7 +65,7 @@ const countHandler = () => {
 const showLoader = () => {
     poetsContentContainer.classList.add('hidden')
     loaderContainer.innerHTML = '<div class="loader w-4 rounded-full animate-loader aspect-square"></div>';
-
+    loaderContainer.classList.add('h-160')
 }
 const hideLoader = () => {
     poetsContentContainer.classList.remove('hidden')
@@ -76,6 +73,62 @@ const hideLoader = () => {
     loaderContainer.classList.remove('h-160')
 }
 
+//sort poets 
+const sortPoetsHandler = (event) => {
+    const totalPoetsItem = event.target.closest('.total-poets')
+    const items = event.target.closest('.sort-by-century-items')
+    loadMorePoetsBtn.classList.add('hidden')
+    if (items) {
+        const currentActiveItem = document.querySelector('.sort-by-century-items.active')
+        if (event.target === currentActiveItem) return;
+        if (currentActiveItem) {
+            currentActiveItem.classList.remove('active');
+            event.target.classList.add('active')
+            fetchSortData(event.target.dataset.century);
+            sortContainer.classList.add('hidden');
+            sortBtn.textContent = event.target.textContent;
+        }
+        if (totalPoetsItem) {
+            count = 1;
+            fetchPoetsData();
+            poetsElementHandler();
+            loadMorePoetsBtn.classList.remove('hidden')
+            return;
+        }
+    }
+}
+
+const fetchSortData = (century) => {
+    showLoader();
+    fetch('https://api.ganjoor.net/api/ganjoor/centuries')
+        .then(res => res.json())
+        .then(data => {
+            console.log(century);
+            const clickedCentury = data.find(item => item.name === century);
+            totalpoets = clickedCentury.poets;
+            poetsElementHandler();
+
+        })
+        .catch(() => { console.log('There is a problem') })
+        .finally(() => {
+            hideLoader();
+        })
+}
+
+const sortContainerOpener = () => {
+    sortContainer.classList.remove('hidden')
+}
+const sortCloseHandler = (event) => {
+    const sortOpen = event.target.closest('.sorte-btn')
+    const items = event.target.closest('.sort-by-century-items')
+    const container = event.target.closest('.sort-container')
+    if (items || container) return
+    if (sortOpen) {
+        sortContainerOpener();
+        return;
+    }
+    sortContainer.classList.add('hidden')
+}
 
 //! theme Handler
 const themeHandler = (isDarkMode) => {
@@ -107,12 +160,11 @@ const saveThemeInLocalStorage = (isDarkMode) => {
     localStorage.setItem('isDarkMode', isDarkMode)
 }
 
+document.addEventListener('click', sortCloseHandler)
 
 document.addEventListener('DOMContentLoaded', onPageLoad)
 darkBtn.addEventListener('click', darkTheme)
 lightBtn.addEventListener('click', lightTheme)
 loadMorePoetsBtn.addEventListener('click', countHandler)
-sortContainer.addEventListener('click', function(){
-    console.log('s');
-    
-})
+sortContainer.addEventListener('click', sortPoetsHandler)
+sortBtn.addEventListener('click', sortContainerOpener)
