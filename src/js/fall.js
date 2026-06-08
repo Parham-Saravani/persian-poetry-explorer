f91858f724371bf211688a15bd59dfe9ce20c965
Parham Saravani <parhamsaravani83@gmail.com>
2026-06-08 18:44:57 +0330

    fallPage is full responsive

--- src/js/fall.js
+++ src/js/fall.js
@@ -21,6 +21,8 @@ const loaderContainer = document.querySelector('.loader-container')
 const errorElement = document.querySelector('.error-element')
 
 let isDone = false;
+
+
 const onPageLoad = () => {
     themeHandler();
 }
@@ -30,11 +32,15 @@ const fetchFall = () => {
     fetch('https://api.ganjoor.net/api/ganjoor/hafez/faal')
         .then(res => res.json())
         .then(data => {
-            isDone = true;
+            console.log(data);
+
             createFallHandler(data);
+            isDone = true;
         })
-        .catch(() => {
-            isDone = false;
+        .catch((error) => {
+            console.log(error);
+
+            isDone = false
             errorHandler();
         })
         .finally(() => {
@@ -49,11 +55,31 @@ const createFallHandler = (data) => {
     poetNumberText.insertAdjacentHTML('beforeend', `${data.title} از دیوان حافظ`)
     poetText.insertAdjacentHTML('beforeend', data.htmlText)
     poetSummeryText.insertAdjacentHTML('beforeend', data.poemSummary)
+    const poets = document.querySelectorAll('.b')
+    poets.forEach(poet => {
+        const firstChildText = poet.firstElementChild.textContent.trim();
+        const secondChildText = poet.lastElementChild.textContent.trim();
 
+        poet.innerHTML = '';
+        const container = document.createElement('div')
+        container.className = 'relative flex items-center justify-center';
+
+        container.insertAdjacentHTML('beforeend', `<span class="right-poet">${firstChildText}</span> <span class="divider">*</span> <span class="left-poet">${firstChildText}</span>`)
+
+        const divider = container.querySelector('.divider')
+        divider.className += 'absolute font-bold text-xl dark:text-dark-primary text-light-primary font-vazir';
+
+        const rightPoet = container.querySelector('.right-poet')
+        rightPoet.className += 'block w-80 absolute -right-90 font-vazir max-xl:-right-76 max-xl:w-70 max-xl:text-[13px] max-lg:text-[15px] max-md:text-[12px] max-md:w-56 max-md:-right-70 max-sm:text-[10px] max-sm:-right-50 max-sm:w-40'
+
+        const leftPoet = container.querySelector('.left-poet')
+        leftPoet.className += 'block w-80 absolute -left-110 font-vazir max-xl:-left-86 max-xl:w-70 max-xl:text-[13px] max-lg:text-[15px] max-md:text-[12px] max-md:w-56 max-md:-left-75 max-sm:text-[10px] max-sm:-left-53 max-sm:w-40';
+
+        poet.append(container)
+    })
 }
 
 const errorHandler = () => {
-    console.log(isDone);
     fallSummaryContent.classList.add('hidden')
     fallSummaryContainer.classList.remove('hidden')
     errorElement.classList.remove('hidden')
