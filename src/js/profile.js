31d844f165d5277bc657ff5077baa3ea570afe4e
Parham Saravani <parhamsaravani83@gmail.com>
2026-06-14 00:39:23 +0330

    profile page is done whithout mobile menu

--- src/js/profile.js
+++ src/js/profile.js
@@ -7,30 +7,60 @@ const exitModal = document.querySelector('.exit-modal')
 const closeExitModal = document.querySelector('.close-exit-modal')
 const exitAccountBtn = document.querySelector('.exit-account')
 
+
+const mainPage = document.querySelector('.main-profile-page')
+const fallHistoryPage = document.querySelector('.fall-history-page')
+const settingPage = document.querySelector('.setting-page')
+// setting checkboxes
+const darkModeCheckbox = document.querySelector('.dark-mode-checkbox')
+
 // menu items
 const menuItems = document.querySelectorAll('.menu-items')
 
 const onPageLoad = () => {
     themeHandler()
+    darkModeHandler()
 }
 
 //? add active class to the clicked menu item
 const activeClickedMenuItem = (event) => {
     const currentActiveItem = event.target.closest('.active')
-    if(currentActiveItem) {
+    if (currentActiveItem) {
         console.log('nothing happend');
-        
+
         return;
-    }else{        
+    } else {
+        const page = event.target.dataset.page;
         menuItems.forEach(item => {
-            if(item.classList.contains('active')){
+            if (item.classList.contains('active')) {
                 item.classList.remove('active')
             }
         })
         event.target.classList.add('active')
+        showClickedMenuItemPage(page)
     }
 }
 
+const showClickedMenuItemPage = (page) => {
+    switch (page) {
+        case 'mainPage': {
+            removeHiddenClassFromActivePage(mainPage, fallHistoryPage, settingPage)
+            break;
+        }
+        case 'fallHistoryPage': {
+            removeHiddenClassFromActivePage(fallHistoryPage, mainPage, settingPage)
+            break;
+        }
+        case 'settingPage': {
+            removeHiddenClassFromActivePage(settingPage, mainPage, fallHistoryPage)
+        }
+    }
+}
+const removeHiddenClassFromActivePage = (active, hide, secondhide) => {
+    active.classList.remove('hidden')
+    hide.classList.add('hidden')
+    secondhide.classList.add('hidden')
+}
 //! modal
 const showModal = () => {
     exitModal.classList.remove('hidden')
@@ -77,6 +107,23 @@ const exitAccount = () => {
     localStorage.removeItem('userData');
     goHomePage()
 }
+
+//! setting page checkbox 
+const darkModeHandler = () => {
+    const isDarkMode = JSON.parse(localStorage.getItem('isDarkMode'))
+    if (isDarkMode) {
+        darkModeCheckbox.checked = true
+    }
+}
+const darkModeCheckboxHandler = () => {
+    console.log('click');
+    if (darkModeCheckbox.checked) {
+        darkTheme()
+    } else {
+        lightTheme()
+    }
+}
+darkModeCheckbox.addEventListener('click', darkModeCheckboxHandler)
 document.addEventListener('keydown', hideModalWithEscapeKey)
 document.addEventListener('DOMContentLoaded', onPageLoad)
 openExitModal.addEventListener('click', showModal)
